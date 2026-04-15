cdeb2bbbab54b0763be1c3b259fb978080bfe1a3
linux00 <pas.cerullo01@gmail.com>
2026-04-15 17:13:44 +0200

    change secret key

--- mysaas/mysaas/settings.py
+++ mysaas/mysaas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8xl#8dw3n4n!=(5y_avq803ql%&c!%1op1oqc%io_y#qz6^$oy'
 
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
